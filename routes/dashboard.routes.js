67670136368b10e01743308034fd185043ff72a8
Filippo Marinolli <f.marinolli@gmail.com>
2023-07-27 22:01:49 +0200

    doneeeee

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -5,7 +5,7 @@ const {isAuthenticated} = require('../middleware/jwt.middleware')
 // const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 const { getNotes, createNotes, updateNote, deleteNote } = require("../controllers/notesController.js");
 
-
+const { getProject, createProject, updateProject, deleteProject } = require("../controllers/projectController.js");
 
 
 router.route("/notes").get(isAuthenticated, getNotes);
@@ -13,6 +13,15 @@ console.log('Request reached /dashboard endpoint');
 router.route("/notes").post(isAuthenticated, createNotes);
 console.log('Request reached /dashboard endpoint');
 router.route("/notes/:id").put(isAuthenticated, updateNote).delete(isAuthenticated, deleteNote);
+console.log('Request reached /dashboard endpoint');
+
+router.route("/project").get(isAuthenticated, getProject);
+console.log('Request reached /dashboard endpoint');
+router.route("/project").post(isAuthenticated, createProject);
+console.log('Request reached /dashboard endpoint');
+router.route("/project/:id").put(isAuthenticated, updateProject).delete(isAuthenticated, deleteProject);
+console.log('Request reached /dashboard endpoint');
+router.route("/project/move").put(isAuthenticated, updateProject);
 console.log('Request reached /dashboard endpoint');
 
 
